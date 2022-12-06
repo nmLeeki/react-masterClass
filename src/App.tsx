af86104306391d6ba90ca09bdbc3c75182f1c85d
nmLeeki <100739132+nmLeeki@users.noreply.github.com>
2022-12-06 17:36:59 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import Circle from "./Circle";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.div`
+    color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  return (
-    <div>
-    <Circle bgColor="teal" />
-    <Circle bgColor="tomato" />
-  </div>
-  );
+    return (
+        <Container>
+            <H1>I'm glutt</H1>
+        </Container>
+    );
 }
 
 export default App;
